f485dfb1c6bed2824453027ce5aa6ff0cd7514d8
pulkit3010 <pulkit3010@gmail.com>
2019-03-27 23:27:43 +0530

    Initial commit

---
+++ src/com/pulkit/Main.java
@@ -0,0 +1,32 @@
+package com.pulkit;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+
+        List<Integer> ints = new ArrayList<Integer>();
+
+        while(true){
+            System.out.println("Enter number");
+            boolean isInt = scanner.hasNextInt();
+
+            if(isInt){
+                int number = scanner.nextInt();
+
+                ints.add(number);
+            }
+
+            if(isInt == false){
+                System.out.println("Oops!Invalid output");
+                break;
+            }scanner.nextLine();
+        }scanner.close();
+        System.out.println("\nThe largest number you entered is \n"+Collections.max(ints));
+
+
+    }}
+
